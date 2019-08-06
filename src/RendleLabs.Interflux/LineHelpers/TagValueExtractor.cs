e715b0e7d539687bb91d72c8d56ff5c8df08692e
Mark Rendle <mark@rendlelabs.com>
2019-08-06 14:27:31 +0100

    LineHelpers

---
+++ src/RendleLabs.Interflux/LineHelpers/TagValueExtractor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace RendleLabs.Interflux.LineHelpers
+{
+    public static class TagValueExtractor
+    {
+        private const byte Space = (byte) ' ';
+        private const byte Comma = (byte) ',';
+        private const byte EqualsSign = (byte) '=';
+        private const byte Backslash = (byte) '\\';
+
+        public static bool TryGetTag(this in ReadOnlySpan<byte> span, in ReadOnlySpan<byte> tag, out ReadOnlySpan<byte> value)
+        {
+            int space = IndexOfUnescaped(span, Space);
+            if (space < 0)
+            {
+                value = default;
+                return false;
+            }
+
+            while (span[space - 1] == Backslash && span[space - 2] != Backslash)
+            {
+                space += span.Slice(space + 1).IndexOf(Space) + 1;
+            }
+
+            var slice = span.Slice(0, space);
+            int comma = slice.IndexOf(Comma);
+            if (comma < 0)
+            {
+                value = default;
+                return false;
+            }
+
+            while (comma > 0)
+            {
+                slice = slice.Slice(comma + 1);
+                int equals = slice.IndexOf(EqualsSign);
+                if (equals > 0)
+                {
+                    var key = slice.Slice(0, equals);
+                    if (key.SequenceEqual(tag))
+                    {
+                        var rest = slice.Slice(equals + 1);
+                        int end = IndexOfUnescaped(rest, Comma);
+                        value = end < 0 ? rest : rest.Slice(0, end);
+                        return true;
+                    }
+                }
+
+                comma = slice.IndexOf(Comma);
+            }
+
+            value = default;
+            return false;
+        }
+
+        private static int IndexOfUnescaped(in ReadOnlySpan<byte> span, byte token)
+        {
+            int index = span.IndexOf(token);
+            if (index < 1) return index;
+            if (span[index - 1] != Backslash) return index;
+            if (index > 1 && span[index - 2] == Backslash) return index;
+            int next = IndexOfUnescaped(span.Slice(index + 1), token);
+            return next < 0 ? next : index + next + 1;
+        }
+    }
+}
---
+++ test/RendleLabs.Interflux.UnitTests/TagValueExtractorTests.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Text;
+using RendleLabs.Interflux.LineHelpers;
+using Xunit;
+
+namespace RendleLabs.Interflux.UnitTests
+{
+    public class TagValueExtractorTests
+    {
+        [Fact]
+        public void ExtractsSimpleValueWhenOneTag()
+        {
+            const string test = "m,x=Foo y=1";
+            var bytes = Encoding.UTF8.GetBytes(test);
+            var key = new[] {(byte) 'x'};
+            var expected = Encoding.UTF8.GetBytes("Foo");
+            Assert.True(TagValueExtractor.TryGetTag(bytes, key, out var actual));
+            var debug = Encoding.UTF8.GetString(actual);
+            Assert.True(expected.AsSpan().SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void ExtractsSimpleValueWhenTwoTags()
+        {
+            const string test = "m,x=Foo,z=Bar y=1";
+            var bytes = Encoding.UTF8.GetBytes(test);
+            var key = new[] {(byte) 'x'};
+            var expected = Encoding.UTF8.GetBytes("Foo");
+            Assert.True(TagValueExtractor.TryGetTag(bytes, key, out var actual));
+            var debug = Encoding.UTF8.GetString(actual);
+            Assert.True(expected.AsSpan().SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void ExtractsSimpleValueWhenThreeTags()
+        {
+            const string test = "m,a=Wibble,x=Foo,z=Bar y=1";
+            var bytes = Encoding.UTF8.GetBytes(test);
+            var key = new[] {(byte) 'x'};
+            var expected = Encoding.UTF8.GetBytes("Foo");
+            Assert.True(TagValueExtractor.TryGetTag(bytes, key, out var actual));
+            var debug = Encoding.UTF8.GetString(actual);
+            Assert.True(expected.AsSpan().SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void ExtractsValueWithEscapedSpace()
+        {
+            const string test = "m,x=Foo\\ Bar y=1";
+            var bytes = Encoding.UTF8.GetBytes(test);
+            var key = new[] {(byte) 'x'};
+            var expected = Encoding.UTF8.GetBytes("Foo\\ Bar");
+            Assert.True(TagValueExtractor.TryGetTag(bytes, key, out var actual));
+            var debug = Encoding.UTF8.GetString(actual);
+            Assert.True(expected.AsSpan().SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void ExtractsValueWithEscapedComma()
+        {
+            const string test = "m,x=Foo\\,Bar y=1";
+            var bytes = Encoding.UTF8.GetBytes(test);
+            var key = new[] {(byte) 'x'};
+            var expected = Encoding.UTF8.GetBytes("Foo\\,Bar");
+            Assert.True(TagValueExtractor.TryGetTag(bytes, key, out var actual));
+            var debug = Encoding.UTF8.GetString(actual);
+            Assert.True(expected.AsSpan().SequenceEqual(actual));
+        }
+    }
+}
